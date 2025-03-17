a3f000211a8214a5b91bb6743cd62b3558a34b76
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-03-17 19:48:06 +0300

    add validation

---
+++ validation/authValidation.js
@@ -0,0 +1,28 @@
+const Joi = require('joi');
+
+// Register validation
+const registerValidation = (data) => {
+    const schema = Joi.object({
+        name: Joi.string().min(6).required(),
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required(),
+        role: Joi.string().valid('admin', 'librarian', 'member').required(),
+        gender: Joi.string().valid('male', 'female').required(),
+        phone: Joi.number().required()
+    });
+    return schema.validate(data, { abortEarly: false });
+};
+
+// Login validation
+const loginValidation = (data) => {
+    const schema = Joi.object({
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required(),
+    });
+    return schema.validate(data, { abortEarly: false });
+};
+
+module.exports = {
+    registerValidation,
+    loginValidation
+}
